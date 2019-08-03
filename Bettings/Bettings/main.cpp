5bb36f8d0ecc666612bcad2fd56ec07adae47dda
Iv-Umlore <iv.kiselev1998@yandex.ru>
2019-08-03 12:05:49 +0300

    full tests

--- Bettings/Bettings/main.cpp
+++ Bettings/Bettings/main.cpp
@@ -15,17 +15,49 @@ int main() {
 	Network NW;
 	string way;
 	if (answer == 'S') {
-		way = "D:\\PROGRAMS\\NeiroBetting\\Bettings\\USA_USL_ChampShip.txt";
+		vector<Match> matches;
+		way = "D:\\PROGRAMS\\NeiroBetting\\Bettings\\USA.txt";
 		ifstream in(way);
 		int count;
+		int MatchCount;
 		int balls1, balls2;
 		in >> count;
-		Match* matches = new Match[count];
 		for (int i = 0; i < count; i++) {
-			matches[i] = Match(in);
+			matches.push_back(Match(in));
 			in >> balls1 >> balls2;
 			matches[i].SetMatchResult(balls1, balls2);
 		}
+		in.close();
+
+		way = "D:\\PROGRAMS\\NeiroBetting\\Bettings\\Japan.Best.txt";
+		in.open(way);
+		in >> count;
+		MatchCount = matches.size();
+		for (int i = MatchCount; i < count + MatchCount; i++) {
+			matches.push_back(Match(in));
+			in >> balls1 >> balls2;
+			matches[i].SetMatchResult(balls1, balls2);
+		}
+		in.close();
+
+		way = "D:\\PROGRAMS\\NeiroBetting\\Bettings\\Russia.txt";
+		in.open(way);
+		in >> count;
+		MatchCount = matches.size();
+		for (int i = MatchCount; i < count + MatchCount; i++) {
+			matches.push_back(Match(in));
+			in >> balls1 >> balls2;
+			matches[i].SetMatchResult(balls1, balls2);
+		}
+		in.close();
+		int* bester = new int[2];
+		int* bester2 = new int[2];
+		bester[0] = 0;
+		bester[1] = 0;
+		int barier = 0;
+		double parametr = 0.1;
+		double learningspeed = 1;
+		cout << "Tests count = " << matches.size() << endl;
 		for (int i = 0; i < count; i++)
 			NW.VangaLerning(matches[i].lastResults, matches[i].CorrectResult);	// учим
 	}
